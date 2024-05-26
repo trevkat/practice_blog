cf5bc839266f8eefb5fc841448f3549c320d2359
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-05-26 08:27:46 -0500

    Added ability to comment on articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
+# Rails.application.routes.draw do
+#   root "articles#index"
+
+#   resources :articles
+
+#   get "/articles", to: "articles#index"
+#   get "/articles/:id", to: "articles#show"
+#   get "up" => "rails/health#show", as: :rails_health_check
+# end
+
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :articles do
+    resources :comments
+  end
 end
+
